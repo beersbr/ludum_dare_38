672666604a23cac18a47fd3d0d5a3f3843b69fb2
Brett Beers <brettbeers@freya.local>
2017-04-21 19:15:36 -0700

    updated bild file

--- build.sh
+++ build.sh
@@ -9,6 +9,10 @@ THIRDPARTY_DIR=$BASE_DIR"/thirdparty"
 # -F framework include dir
 # -I include dir
 
+cp -r thirdparty/frameworks/* $BUILD_DIR/
+
 pushd $SRC_DIR
-	clang++ main.cpp -I$THIRDPARTY_DIR"/include" -F$THIRDPARTY_DIR"/frameworks" -framework SDL2 -framework opengl -o $BUILD_DIR"/ludum_dare_38" 
+	clang++ main.cpp -I$THIRDPARTY_DIR"/include" -F$THIRDPARTY_DIR"/frameworks" -framework SDL2 -framework opengl -o $BUILD_DIR"/ludum_dare_38" \
+	-rpath @executable_path/ \
+	-D SLOW
 popd

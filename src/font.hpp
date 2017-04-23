8d651823d1922d44f1f17cdd5dc6fd1ff9b0b029
Brett Beers <brettbeers@freya.local>
2017-04-23 15:00:40 -0700

    more font updates

--- src/font.hpp
+++ src/font.hpp
@@ -2,13 +2,17 @@
 #define FONT_HPP
 
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 #include <ft2build.h>
 #include FT_FREETYPE_H
 
+#include "util.hpp"
 #include "graphics.hpp"
 
 #include <map>
 // #include <pair>
+
+// NOTE(Brett):https://learnopengl.com/#!In-Practice/Text-Rendering
 
 #define FONT_SHADER_ATTRIBUTE_POSITION 0
 #define FONT_SHADER_ATTRIBUTE_UV 1
@@ -22,14 +26,18 @@ typedef struct _font_vertex_description_t {
 
 typedef struct _glyph_t {
     texture_t texture;
-    glm::vec2 size;
-    glm::vec2 baseline_offset;
+    glm::ivec2 size;
+    glm::ivec2 baseline_offset;
     GLuint glyph_offset;
 } glyph_t;
 
 
 void setup_text_renderer(shader_t *shader, char const * font_path);
-void redner_text(std::string text, float x, float y, float scale, glm::vec3 color);
+void render_text(std::string text,
+                 glm::vec3 position,
+                 glm::vec3 color,
+                 float scale, glm::mat4 projection,
+                 glm::mat4 view = glm::mat4(1.0f) );
 
 
 #endif
